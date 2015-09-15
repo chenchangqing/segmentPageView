ef9dd38091806d7a8645b55915f4ecd7960fb06c
changqing <chenchangqing198@126.com>
2015-09-15 12:53:22 +0800

    add segment

--- SSASideMenu+HMSegment+SwipeView/SSASideMenu+HMSegment+SwipeView/ViewController.swift
+++ SSASideMenu+HMSegment+SwipeView/SSASideMenu+HMSegment+SwipeView/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - HMSegmentedControl
+    
+    var segmentedControl: HMSegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUp()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Set Up
+    
+    private func setUp() {
+        
+        setUpSegmentedControl()
     }
-
-
+    
+    // MARK: - Set Up SegmentedControl
+    
+    private func setUpSegmentedControl() {
+        
+        segmentedControl = HMSegmentedControl(frame: CGRectMake(0, 64, UIScreen.mainScreen().bounds.size.width, 40))
+        segmentedControl.sectionTitles               = ["景点","餐饮","购物","酒店","活动"]
+        segmentedControl.selectionIndicatorHeight    = 2.0
+        segmentedControl.selectionIndicatorColor     = UIColor.blueColor()
+        segmentedControl.selectionIndicatorLocation  = HMSegmentedControlSelectionIndicatorLocationDown
+        segmentedControl.backgroundColor             = UIColor.lightGrayColor()
+        segmentedControl.titleTextAttributes         = [NSFontAttributeName:UIFont.systemFontOfSize(14)]
+        segmentedControl.shouldAnimateUserSelection  = true
+        segmentedControl.selectionStyle              = HMSegmentedControlSelectionStyleFullWidthStripe
+        
+        self.view.addSubview(segmentedControl)
+    }
 }
 
